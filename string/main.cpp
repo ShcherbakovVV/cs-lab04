de66d60db51c06ec4c3f4dafbec1f983cf361ca0
ShcherbakovVV <48246768+ShcherbakovVV@users.noreply.github.com>
2019-05-14 00:03:10 +0300

    code: update

--- string/main.cpp
+++ string/main.cpp
@@ -6,7 +6,6 @@ using namespace std;
 
 const size_t MAX_SIZE = 256;
 const size_t MAX_EXTENSION = 3;
-const char* separators = " \r\n,.!?:;()-";
 const char* forbidden =  "*<>?|\"";
 
 int main()
@@ -15,84 +14,82 @@ int main()
     cout << "Enter file name: ";
     fgets(text, MAX_SIZE, stdin);
     char* filename = text;
-    while(true)
-    {
+    while(true) {
         if( strcspn( filename, forbidden ) == strlen( filename ) ) ;
-        else
-        {
-            cout << "Invalid format\n";
+        else {
+            cout << "Invalid format";
             break;
         }
 
         if( strchr( filename, ':' ) == nullptr ) ;
-        else
-        {
+        else {
             char* colon = strchr(filename,':');
-            if( colon - filename + 1 <= 1 )
-            {
+            if( colon - filename + 1 <= 1 ) {
                 cout << "Invalid format";
                 break;
             }
-            while( colon != nullptr )
-            {
+            while( colon != nullptr ) {
                 if( isalpha( filename[colon - filename - 1] ) > 0 &&
                     filename[colon - filename + 1] == '\\')
                     colon = strchr(colon + 1, ':');
-                else
-                {
+                else {
                     cout << "Invalid format";
                     break;
                 }
             }
         }
 
-        if( strrchr( filename, '.' ) == nullptr )
-        {
-            *(filename + strlen(filename) - 1) = '.';
+        if( strrchr( filename, '.' ) == nullptr ) {
+            *(filename + strlen(filename) - 1) = '.';                  //strcat не работает
             strcat( filename, "txt" );
         }
 
-        else
-        {
+        else {
             char* ext = strrchr( filename, '.' );
             for( int i = 1; ext[i] != '\0'; i ++ )
                 *(ext + i) = char(tolower(*(ext + i)));
-            if( strncmp( ext, ".txt", MAX_EXTENSION + 1 ) == 0 );
-            else
-            {
+            if( strncmp( ext, ".txt", MAX_EXTENSION + 1 ) == 0 ) ;
+                //*( filename + strlen(filename) - 1 ) = '\0';              //problem
+            else {
                 cout << "Invalid format";
                 break;
             }
         }
-        break;
-    }
-    cout << filename << "\n" << strstr( "text text apple", "apple" );
 
-    FILE* file = fopen( filename, "r" );
-    fseek( file, 0, SEEK_END );
-    long filesize = ftell( file );
-
-    char* filedata = new char[filesize];
-    rewind( file );                                // ошибка в этом блоке
-    fread( filedata, 1, filesize, file );
-
-    /*char STR[MAX_SIZE];
-    cout << "Enter string: ";
-    fgets(STR, MAX_SIZE, stdin);
-    const char* str = STR;
-
-    //int occur = 0;
-    while(true)
-    {
-        if( strstr( filedata, str ) != nullptr )
-            cout << strstr( filedata, str );
-        else
-        {
-            cout << "NULL";
+        FILE* file = fopen( filename, "r" );
+        if( file == nullptr ) {
+            cout << "Invalid file name";
             break;
         }
-    }*/
-    fclose( file );
-    //delete[] filedata;
+        fseek( file, 0, SEEK_END );
+        long filesize = ftell( file );
+
+        char* filedata = new char[filesize];
+        rewind( file );
+        fread( filedata, 1, filesize, file );
+        char* fldt = filedata;
+        *( fldt + strlen(fldt) - 1 ) = '\0';
+
+        cout << fldt;                              //problem
+
+        char STR[MAX_SIZE];
+        cout << "Enter string: ";
+        fgets(STR, MAX_SIZE, stdin);
+        char* str = STR;
+        *( str + strlen(str) - 1 ) = '\0';
+        //cout << str;             //problem
+
+        int occur = 0;
+        while( strstr( fldt, str ) != nullptr ) {
+            occur ++;
+            //cout << strstr( filedata, str );
+            fldt = strstr( fldt, str ) + 1;
+            cout << fldt;
+        }
+        cout << "Occurance number: " << occur;
+        delete[] filedata;
+        fclose( file );
+        break;
+    }
     return 0;
 }
