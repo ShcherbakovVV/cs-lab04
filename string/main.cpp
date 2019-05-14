bcd96aaf1e0de9b8f221d4526b2f8f409b9c4791
ShcherbakovVV <48246768+ShcherbakovVV@users.noreply.github.com>
2019-05-14 14:34:07 +0300

    code: update 2

--- string/main.cpp
+++ string/main.cpp
@@ -14,6 +14,7 @@ int main()
     cout << "Enter file name: ";
     fgets(text, MAX_SIZE, stdin);
     char* filename = text;
+    filename[strlen(filename) - 1] = '\0';
     while(true) {
         if( strcspn( filename, forbidden ) == strlen( filename ) ) ;
         else {
@@ -39,22 +40,21 @@ int main()
             }
         }
 
-        if( strrchr( filename, '.' ) == nullptr ) {
-            *(filename + strlen(filename) - 1) = '.';                  //strcat не работает
-            strcat( filename, "txt" );
-        }
+        if( strrchr( filename, '.' ) == nullptr )
+            strcat( filename, ".txt" );
 
         else {
             char* ext = strrchr( filename, '.' );
             for( int i = 1; ext[i] != '\0'; i ++ )
                 *(ext + i) = char(tolower(*(ext + i)));
             if( strncmp( ext, ".txt", MAX_EXTENSION + 1 ) == 0 ) ;
-                //*( filename + strlen(filename) - 1 ) = '\0';              //problem
             else {
                 cout << "Invalid format";
                 break;
             }
         }
+
+        puts(filename);
 
         FILE* file = fopen( filename, "r" );
         if( file == nullptr ) {
@@ -68,23 +68,18 @@ int main()
         rewind( file );
         fread( filedata, 1, filesize, file );
         char* fldt = filedata;
-        *( fldt + strlen(fldt) - 1 ) = '\0';
-
-        cout << fldt;                              //problem
+        fldt[filesize  ] = '\0';
 
         char STR[MAX_SIZE];
         cout << "Enter string: ";
         fgets(STR, MAX_SIZE, stdin);
         char* str = STR;
         *( str + strlen(str) - 1 ) = '\0';
-        //cout << str;             //problem
 
         int occur = 0;
         while( strstr( fldt, str ) != nullptr ) {
             occur ++;
-            //cout << strstr( filedata, str );
             fldt = strstr( fldt, str ) + 1;
-            cout << fldt;
         }
         cout << "Occurance number: " << occur;
         delete[] filedata;
